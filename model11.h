82cd9e193ce99b0da0d18a661610afefd5e33804
ghibilines <117523270+ghibilines@users.noreply.github.com>
2022-11-08 16:55:08 +0000

    Update model11.h

--- model11.h
+++ model11.h
@@ -4,17 +4,45 @@
 #include <math.h>
 struct node     {
         node *lefttop;
-	node *leftlow;
+        node *leftlow;
         node *right;
         double value;
-	double valuetop;
-	double valuelow;
+        double valuetop;
+        double valuelow;
         double weighttop;
-	double weightlow;
-        double applytop() {
-                return lefttop->value*weighttop;
+        double weightlow;
+        void connect(node *lt, node *ll, node *r)       {
+                lefttop = lt;
+                leftlow = ll;
+                right = r;
         }
-	double applylow() {
-		return leftlow->value*weightlow;
-	}
+        void applytop() {
+                valuetop = lefttop->value*weighttop;
+        }
+        void applylow() {
+                valuelow = leftlow->value*weightlow;
+        }
+        void correcttop(double b, double c) {
+                weighttop -= 0.01*((b-c)/(c))*(valuetop/valuelow)*(weighttop/fabs(weighttop));
+        }
+        void correctlow(double b, double c) {
+                weightlow -= 0.01*((b-c)/(c))*(valuelow/valuetop)*(weightlow/fabs(weightlow));
+        }
+        void assigninit(double a)       {
+                weighttop = a;
+                weightlow = a;
+        }
 };
+void printmodel(node *p, int a);
+
+void printmodel(node *p, int a)
+{
+        int i;
+        printf("\nmodel:\n");
+        for(i=0;i<a;i++)        {
+                printf("node%d\tweighttop:%f, weightlow: %f\n",i, (p+i)->weighttop, (p+i)->weightlow);
+        }
+        return;
+
+
+}
